e00ef09e550b2921e8ddccd8c9d7bb02dd92b630
student <bettyjcawthon@gmail.com>
2019-04-21 10:13:57 -0500

    fixed home page 404 error

--- About-Me.js
+++ About-Me.js
@@ -1,9 +1,8 @@
-//store a reference to the button inside a variable called btn
-var btn = document.querySelector('button');
-//Defined a function that returns a random number.
-function random(number) {
-    return Math.floor(Math.random()*(number+1));
-}
+//Function to allow the background color to change with the click of a button
+btn.onclick = function() {
+    var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+    document.body.style.backgroundColor = rndCol;
+}}
 
 //Function to allow the background color to change with the click of a button
 btn.onclick = function() {
